6b43aaa5c67c8245409fbad0672418735e55b4e6
jowend <joaquinvcestudio@gmail.com>
2024-01-23 20:45:17 -0500

    random number

--- 02-logica-de-programacion-funciones-listas/app.js
+++ 02-logica-de-programacion-funciones-listas/app.js
@@ -1,11 +1,22 @@
+let randomNumber = generateRandomNumber();
+
+console.log(randomNumber)
+
 function intentoDeUsuario() {
     alert('Click desde la funcion');
+    return;
 }
 
 function assignTextToElement(element, text) {
     let title = document.querySelector(element);
 
     title.innerHTML = text;
+    return;
+}
+
+function generateRandomNumber(params) {
+    let randomNumber = Math.floor(Math.random()*10)+1
+    return randomNumber;
 }
 
 assignTextToElement('h1','Juego del numero secreto');
